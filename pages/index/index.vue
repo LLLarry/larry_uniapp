30242f3db70a7f754a13c4a37c4c28ff0d750b65
LLLarry <1079485906@qq.com>
2019-12-13 22:57:00 +0800

    12/13/22

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -16,6 +16,15 @@
 				走出去，才发现你跟别人差的不是一点半点
 			</text>
 			<view class="middle">
+				<view class="play">
+					<view class="iconfont icon-bofang"></view>
+				</view>
+				<view class="play_info">
+					<view class="play_info_item">
+						<text>20W次播放</text>
+						<text>2:47</text>
+					</view>
+				</view>
 				<image src="../../static/demo/datapic/11.jpg" mode="widthFix"></image>
 			</view>
 			<view class="bottom">
@@ -42,87 +51,7 @@
 			</view>
 		</view>
 		
-		<view class="card">
-			<view class="top">
-				<view class="userInfo">
-					<image src="/static/demo/demo5.jpg" mode="widthFix"></image>
-					<text>鲁大师</text>
-				</view>
-				<view class="add">
-					<view class="icon iconfont">&#xe684;</view>
-					<text>关注</text>
-				</view>
-			</view>
-			<text class="title">
-				走出去，才发现你跟别人差的不是一点半点
-			</text>
-			<view class="middle">
-				<image src="../../static/demo/datapic/11.jpg" mode="widthFix"></image>
-			</view>
-			<view class="bottom">
-				<view class="left">
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe650;</view>
-						<view class="icon_list">570</view>
-					</view>
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe600;</view>
-						<view class="icon_list">853</view>
-					</view>
-				</view>
-				<view class="right">
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe60f;</view>
-						<view class="icon_list">570</view>
-					</view>
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe627;</view>
-						<view class="icon_list">853</view>
-					</view>
-				</view>
-			</view>
-		</view>
 		
-		<view class="card">
-			<view class="top">
-				<view class="userInfo">
-					<image src="/static/demo/demo5.jpg" mode="widthFix"></image>
-					<text>鲁大师</text>
-				</view>
-				<view class="add">
-					<view class="icon iconfont">&#xe684;</view>
-					<text>关注</text>
-				</view>
-			</view>
-			<text class="title">
-				走出去，才发现你跟别人差的不是一点半点
-			</text>
-			<view class="middle">
-				<image src="../../static/demo/datapic/11.jpg" mode="widthFix"></image>
-			</view>
-			<view class="bottom">
-				<view class="left">
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe650;</view>
-						<view class="icon_list">570</view>
-					</view>
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe600;</view>
-						<view class="icon_list">853</view>
-					</view>
-				</view>
-				<view class="right">
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe60f;</view>
-						<view class="icon_list">570</view>
-					</view>
-					<view class="icon_item">
-						<view class="icon_list icon iconfont">&#xe627;</view>
-						<view class="icon_list">853</view>
-					</view>
-				</view>
-			</view>
-		</view>
 	</view>
 </template>
 
@@ -186,7 +115,39 @@
 	/* margin: 27upx 0 48upx; */
 }
 .card .middle {
+	height: 380upx;
 	margin-top: 48upx;
+	position: relative;
+}
+.card .middle .play {
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	-webkit-transform: translate(-50%,-50%);
+	-moz-transform:  translate(-50%,-50%);
+	-ms-transform:  translate(-50%,-50%);
+	-o-transform:  translate(-50%,-50%);
+	transform:  translate(-50%,-50%);
+	color: rgba(255,255,255,.85);
+	z-index: 1;
+}
+.card .middle .play_info {
+	position: absolute;
+	right: 10upx;
+	bottom: 10upx;
+	z-index: 1;
+	line-height: 50upx;
+	background-color: rgba(0,0,0,.5);
+	color: #fff;
+	padding: 0 10upx;
+	border-radius: 50upx;
+	font-size: 22upx
+}
+.card .middle .play_info .play_info_item text:first-child {
+	margin-right: 10upx;
+}
+.card .middle .play>view {
+	font-size: 120upx;	
 }
 .card .middle image {
 	width: 100%;
@@ -217,4 +178,5 @@
 .card .bottom .icon_item:nth-child(2){
 	margin-left: 20upx;
 }
+
 </style>
